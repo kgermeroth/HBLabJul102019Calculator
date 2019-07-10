bd44e04e5862b121f3d9c1f95e7478951169a62b
Hackbright Student <no-reply@hackbrightacademy.com>
2019-07-10 16:00:03 -0700

    Successful quit and add options

--- calculator.py
+++ calculator.py
@@ -6,27 +6,34 @@ calculator program yourself in this file.
 
 from arithmetic import *
 
+    #show a list of options:
+print("Here are your options:")
+print("Add or +")
+print("Subtract or -")
+print("Multiply or *")
+print("Divide or /")
+print("Square")
+print("Cube")
+print("Power")
+print("Mod or %")
+print("Quit")
 
 
 # repeat forever:
 while True:
-    #show a list of options:
-    print("Here are your options:")
-    print("Add or +")
-    print("Subtract or -")
-    print("Multiply or *")
-    print("Divide or /")
-    print("Square")
-    print("Cube")
-    print("Power")
-    print("Mod or %")
 
     print("Type your choice followed by integers separated by a space")
 #     read input
-    user_prompt = input("> ")
+    user_prompt = input("> ").lower()
 #     tokenize input
     #
+    token = user_prompt.split(" ") #list of inputs
 #     if the first token is "q":
+    if token[0].startswith("q"):
+        break
 #         quit
 #     else:
 #         decide which math function to call based on first token
+    elif token[0].startswith("add") or token[0] == "+":
+        print(add(int(token[1]), int(token[2])))
+        
