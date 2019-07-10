4c5803e4910907a653f37bd6d7dc6e01eff71a1d
Hackbright Student <no-reply@hackbrightacademy.com>
2019-07-10 14:58:47 -0700

    added multiplication function

--- arithmetic.py
+++ arithmetic.py
@@ -10,11 +10,9 @@ def subtract(num1, num2):
     """Return the second number subtracted from the first."""
     return num1 - num2
 
-print(subtract(5,2))
-
 def multiply(num1, num2):
     """Multiply the two inputs together."""
-
+    return num1 * num2
 
 def divide(num1, num2):
     """Divide the first input by the second and return the result."""
